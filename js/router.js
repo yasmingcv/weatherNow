ab508e3a3ab505e15c143b35f429567491aabafe
Yasmin Gonçalves <yasmingoncalves5421@gmail.com>
2023-05-02 23:54:54 -0300

    Arrumar rotas

---
+++ js/router.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const routes = {
+    '/cidades' : './pages/cidades.html'
+}
+
+const route = async () => {
+    window.event.preventDefault()
+    window.history.pushState({}, "", window.event.target.href)
+
+    const path = window.location.pathname
+
+    const response = await fetch(routes[path])
+    const html = await response.text()
+
+    document.getElementById('root').innerHTML = html
+}
+
+window.route = route
